c6984b21347136ada7446be9fd25e1bf5c68640f
haroune99 <hrna2011@gmail.com>
2025-01-21 22:49:27 +0000

    57...

--- Longuest_Consecutive_Sequence.py
+++ Longuest_Consecutive_Sequence.py
@@ -4,6 +4,13 @@ class Solution:
             first_index = nums.index(0)
             nums = [value for i, value in enumerate(nums) if value != 0 or i == first_index]
 
+        for i in range(1, len(nums)):
+            if nums[i] != nums[0]:
+                break
+            else:
+                return 1 
+
+
         if len(nums) ==0:
             return 0
         elif len(nums) ==1:
@@ -14,19 +21,19 @@ class Solution:
             for i in range(1,(len(sorted_nums))):
                 runs.append(sorted_nums[i]-sorted_nums[i-1])
 
+
             print(runs)
             if runs.count(0) > 1:
                 first_index = runs.index(0)
                 runs = [value for i, value in enumerate(runs) if value != 0 or i == first_index]
 
             cs = []
-            c = 0
             for j in range(len(runs)):
-                while (j < len(runs)) and (runs[j] == 1 or runs[j] == 0):
+                c = 0
+                while (j < len(runs)) and (runs[j] ==1):
                     c+=1
                     j+=1
                 cs.append(c+1)
-                c=0
             
             return max(cs)
 
