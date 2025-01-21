35316e9f3d3ec93266895b30ab43f65a4d10b4e9
haroune99 <hrna2011@gmail.com>
2025-01-21 22:37:28 +0000

    50/79 some progress...

--- Longuest_Consecutive_Sequence.py
+++ Longuest_Consecutive_Sequence.py
@@ -1,5 +1,9 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        if nums.count(0) > 1:
+            first_index = nums.index(0)
+            nums = [value for i, value in enumerate(nums) if value != 0 or i == first_index]
+
         if len(nums) ==0:
             return 0
         elif len(nums) ==1:
@@ -10,10 +14,15 @@ class Solution:
             for i in range(1,(len(sorted_nums))):
                 runs.append(sorted_nums[i]-sorted_nums[i-1])
 
+            print(runs)
+            if runs.count(0) > 1:
+                first_index = runs.index(0)
+                runs = [value for i, value in enumerate(runs) if value != 0 or i == first_index]
+
             cs = []
             c = 0
             for j in range(len(runs)):
-                while (j < len(runs)) and (runs[j] == 1):
+                while (j < len(runs)) and (runs[j] == 1 or runs[j] == 0):
                     c+=1
                     j+=1
                 cs.append(c+1)
