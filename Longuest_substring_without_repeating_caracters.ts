35d7074fa9ca55cfb0036919236955a149aa2382
haroune99 <hrna2011@gmail.com>
2025-01-30 12:42:45 +0000

    Not the optimal solution, but need to use two pointers

--- Longuest_substring_without_repeating_caracters.ts
+++ Longuest_substring_without_repeating_caracters.ts
@@ -1,21 +1,27 @@
 function lengthOfLongestSubstring(s: string): number {
     const arr: number[] = new Array(s.length).fill(0);
-    let temp: string[] = []
-    temp.push(s[0])
-    let count: number = 1
-    for (let i=1; i<s.length; i++){
-        if (!temp.includes(s[i])){
-            count++
-            temp.push(s[i])
+    let temp: string[] = [];
+    let count: number = 0;
+    let maxCount: number = 0;
+    
+    if (s.length === 0) {
+        return 0;
+    }
+    if (s.length === 1) {
+        return 1;
+    }
+
+    for (let i = 0, j = 0; i < s.length; i++) {
+        while (temp.includes(s[i])) {
+            temp.shift(); 
+            count--;
         }
-        else {
-            arr[i] = count
-            count = 1
-            temp = []
-            temp.push(s[i])
-        }
+        temp.push(s[i]);
+        count++;
+        arr[i] = count;
+        maxCount = Math.max(maxCount, count);
     }
     
-    return Math.max(...arr)
-
+    return maxCount;
 }
+
